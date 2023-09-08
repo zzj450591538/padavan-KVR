67f5740a01b125d2a4f22b5617b3b9ffb723b611
zzj450591538 <92283916+zzj450591538@users.noreply.github.com>
2023-09-08 13:52:36 +0800

    Update mtd_storage.sh

--- trunk/user/scripts/mtd_storage.sh
+++ trunk/user/scripts/mtd_storage.sh
@@ -3,7 +3,7 @@
 result=0
 mtd_part_name="Storage"
 mtd_part_dev="/dev/mtdblock5"
-mtd_part_size=12582912
+mtd_part_size=20971520
 dir_storage="/etc/storage"
 slk="/tmp/.storage_locked"
 tmp="/tmp/storage.tar"
